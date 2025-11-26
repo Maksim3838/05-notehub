8eeeca90f73b25d7d376b3b001712d5fd54794ac
Maksim <70702008m@gmail.com>
2025-11-26 16:09:01 +0200

    text

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+import { createPortal } from "react-dom";
 import type { ReactNode } from "react";
 
 interface ModalProps {
@@ -7,22 +9,45 @@ interface ModalProps {
 }
 
 export default function Modal({ isOpen, onClose, children }: ModalProps) {
-  if (!isOpen) return null;
+    const modalRoot =
+    document.getElementById("modal-root") ??
+    (() => {
+      const root = document.createElement("div");
+      root.id = "modal-root";
+      document.body.appendChild(root);
+      return root;
+    })();
+  
+  useEffect(() => {
+    if (!isOpen) return; 
+    const handleEsc = (event: KeyboardEvent) => {
+      if (event.key === "Escape") onClose();
+    };
 
-  return (
+    document.addEventListener("keydown", handleEsc);
+
+       const prevOverflow = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+
+    return () => {
+      document.removeEventListener("keydown", handleEsc);
+      document.body.style.overflow = prevOverflow;
+    };
+  }, [isOpen, onClose]);
+
+    if (!isOpen) return null;
+
+  return createPortal(
     <div
       onClick={onClose}
       style={{
         position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
+        inset: 0,
         background: "rgba(0,0,0,0.5)",
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        zIndex: 999,
+        zIndex: 9999,
       }}
     >
       <div
@@ -36,6 +61,7 @@ export default function Modal({ isOpen, onClose, children }: ModalProps) {
       >
         {children}
       </div>
-    </div>
+    </div>,
+    modalRoot
   );
 }
