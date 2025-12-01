715259cb24ff17ea859323dc6cbc54263dd98138
Maksim <70702008m@gmail.com>
2025-12-01 16:47:31 +0200

    text

--- src/components/NoteForm/NoteForm.tsx
+++ src/components/NoteForm/NoteForm.tsx
@@ -5,8 +5,7 @@ import { createNote } from "../../services/noteService";
 import type { Note } from "../../types/note";
 
 interface NoteFormProps {
-  onClose: () => void;
-   onSuccess: () => void;
+   onSuccess?: () => void;
   onCancel: () => void;
   }
 
